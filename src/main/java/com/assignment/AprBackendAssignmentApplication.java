f3fd907c91008ca2a6e4494a5897085e9ee3e1e3
supra1nova <supra1nova@gmail.com>
2025-10-19 07:35:29 +0900

    project init

---
+++ src/main/java/com/assignment/AprBackendAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprBackendAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AprBackendAssignmentApplication.class, args);
+    }
+
+}
